76e104f78b96ce820db7ef9674294b6f89379306
Malu Repossi <106559836+malu-repossi@users.noreply.github.com>
2023-04-03 11:01:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-import { opcao1, opcao2, opcao3 } from './funcao';
-const input = require('readline-sync');
+import { opcao1, opcao2, opcao3 } from './funcao.js';
+import readlineSync from 'readline-sync';
 
 console.log("Suas opções: \n")
 console.log("1 - Listar os salários minímos de 2010 à 2020")
 console.log("2 - Listar o índice IPCA de 2010 à 2020")
 console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA\n")
 
-escolha = input.question("Digite a opção escolhida:");
-escolha = Number(escolha);
+const escolha = readlineSync.question("Digite a opcao escolhida:");
 
 switch (escolha){
-    case 1:
-        opcao1;
+    case "1":
+        opcao1();
         break;
-    case 2:
-        opcao2;
+    case "2":
+        opcao2();
         break;
-    case 3:        
-        opcao3;
+    case "3":        
+        opcao3();
         break;
     default: 
         console.log("Opção Inválida!");
